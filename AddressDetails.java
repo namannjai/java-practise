522b6c091771605d26be76b1aa5f99aa76d83bc9
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-03-29 12:13:11 +0530

    update

---
+++ AddressDetails.java
@@ -0,0 +1,5 @@
+public class AddressDetails{
+    public static void main(String []args){
+        System.out.println("Door No: D089Street: St. Louis StreetCity: SpringfieldZIP Code: 62729");
+    }
+}
--- gaynu.py
+++
@@ -1,6 +0,0 @@
-class calculator:
-     def  add(a,b=0,c=0):
-        return a+b+c 
-     
-c1=calculator()
-c1.add(2,3)
