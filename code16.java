d2d6e6e8b0783b575a81ba13487bc3e9347afefa
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-03-31 22:39:17 +0530

    new codes

---
+++ code16.java
@@ -0,0 +1,13 @@
+public class code16 {
+	public static void main(String[] args) {
+		int number = 28;
+		for (int num = 25; num < 30; num++) {
+			if (number > num) {
+				System.out.println(num);
+			} else {
+				System.out.println(num);
+				break;
+			}
+		}
+	}
+}
