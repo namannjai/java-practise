0660e4e98a6706c5a0455ab3ff47c62c9057a216
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-04-02 14:34:07 +0530

    new codes 2/4/26

---
+++ chatgpt.java
@@ -0,0 +1,16 @@
+public class chatgpt {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 7;
+        int c = 2;
+
+        
+
+if (c == 7) System.out.println(-1);
+else if (b == 7) System.out.println(c);
+else if (a == 7) System.out.println(b * c);
+else System.out.println(a * b * c);
+
+        
+    }
+}
--- scs3.java
+++ scs3.java
@@ -1,10 +1,34 @@
 public class scs3{
     public static void main(String []args){
-        int a =1;
-        int b =5;
-        int c=3;
+        int a =2;
+        int b =2;
+        int c=7   ;
         if(a==7){
-            System.out.println(b*c);
-        }
-    }
+                 if(b==7){
+                         if(c==7){
+                                System.out.println("-1");
+                          }else{
+                    System.out.println(c);
+                 } 
+                 }else{
+                    System.out.println(b*c);
+                 }
+      } else if (a!=7) {
+                 if(b!=7){
+                         if(c!=7){
+                                System.out.println(a*b*c);
+                          }else{
+                            System.out.println("-1");
+                          }
+                 } else if(c==7){
+                    System.out.println("-1");
+                 }else{
+                    System.out.println(c);
+                 }
+     }else{
+        System.out.println(b*c);
+     }
+      
+       
+}
 }
