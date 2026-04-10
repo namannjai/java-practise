71a1f6a83ca8702e36a0b985a4eff3af891555b8
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-04-10 08:02:16 +0530

    infosys springboard five rupee coin question

--- scs6_1.java
+++ scs6_1.java
@@ -1,9 +1,29 @@
-public class scs6_1{
-public static void main(String[] args) {
-    int oneNoteAvailable;
-    int fiveNoteAvailable;
-    int amount;
-    int fiveUsed;
-    
-}
+public class scs6_1 {
+    public static void main(String[] args) {
+
+        int oneNoteAvailable = 4;
+        int fiveNoteAvailable = 3;
+        int amount = 19;
+
+        int fiveUsed = amount / 5;/*actual number of five note used in solution(kitne 5 ke note use hue hai)*/
+
+        if (fiveUsed > fiveNoteAvailable) {
+            fiveUsed = fiveNoteAvailable;
+        }
+
+        while (fiveUsed >= 0) {
+
+            int remaining = amount - (fiveUsed * 5);
+
+            if (remaining <= oneNoteAvailable) {
+                System.out.println("five rupee notes: " + fiveUsed);
+                System.out.println("one rupee notes: " + remaining);
+                return;
+            }
+
+            fiveUsed--;
+        }
+
+        System.out.println("-1");
+    }
 }
