c7fe9688f8965eefc036ea54727adff30b24333c
namannjai <namanjaiswal1119@gmail.com>
2026-04-14 18:49:54 +0530

    wrong code entry

--- scs7.java
+++ scs7.java
@@ -1,1 +1,15 @@
-public class scs7
+public class scs7{
+    public static void main(String []args){
+        int day =10;
+        int month = 11;
+        int year = 7;
+
+        System.out.print(++day + "-");
+        System.out.print(month +"-");
+        if(year<=9 && year>=0){
+            System.out.print("200"+year);
+        }else{
+            System.out.print("20"+year);
+        }
+    }
+}
