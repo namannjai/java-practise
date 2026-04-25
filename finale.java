ace737fee99d7cbfd54e61f3069fb5e46912d785
namannjai <namanjaiswal1119@gmail.com>
2026-04-25 19:10:28 +0530

    final method

---
+++ finale.java
@@ -0,0 +1,9 @@
+// final = variable method and class
+
+public class finale{
+    public static void main(String[] args) {
+        final int numbr = 89;
+      //  numbr =90; cannot change value due to final keyword
+      System.out.println(numbr);
+    }
+}
