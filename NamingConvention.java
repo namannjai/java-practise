1f9e1a77e537e41327cd6802a5e35ac3e0ccd504
namannjai <namanjaiswal1119@gmail.com>
2026-04-25 15:29:53 +0530

    NamingConvention

---
+++ NamingConvention.java
@@ -0,0 +1,16 @@
+// camel casing is used in java
+/*    the class name is genrelly 1st word capital for example : Hello.java etc
+    the variable name is generally 1st word small for example : int age;
+    the constants are genrally all capital : PIE , MY_DATA
+    constructor like this Human()
+    and all set
+ */
+public class NamingConvention{
+    public static void main(String[] args) {
+        
+        while(true){
+        System.out.println("Naming conventions");
+        return;
+        }
+    }
+}
