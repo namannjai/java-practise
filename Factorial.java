6e8f273f46837aa0633b3b3df856ae6562567f17
namannjai <namanjaiswal1119@gmail.com>
2026-04-26 12:30:24 +0530

    factorial code

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial{
+    public static void main(String[] args) {
+        int sum =1;
+        int n =0;
+        for (int i =1;i<=n;i++){
+            sum = sum *i;
+        }
+        System.out.println(sum);
+    }
+}
